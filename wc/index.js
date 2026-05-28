bcb4da61f8eb956b8964c19ca806841fe6a4beb4
Prateek Mathur <prateekmathur1991@gmail.com>
2026-05-28 09:24:35 +0530

    Byte counting works, but can be improved using IIFE

--- wc/index.js
+++ wc/index.js
@@ -1,6 +1,27 @@
 #!/usr/bin/env node
 
+let fs = require('fs').promises;
+
+const fileStats = async (somePath) => {
+  let stats = await fs.stat(somePath);
+  return stats.size;
+}
+
+async function getFileStats(somePath) {
+  let stats = await fs.stat(somePath);
+  return stats.size;
+}
+
 const args = process.argv.slice(2);
-if (args.length > 0) {
-  console.log(`Arguments received: ${args.join(", ")}`);
+if (args.length != 2) {
+  console.error('Usage: ccwc <flag> <fileName>');
+  process.exit(1);
+}
+
+let flag = args[0];
+let filePath = args[1];
+
+if (flag === '-c') {
+  let thatValue = getFileStats(filePath);
+  thatValue.then((result) => console.log(result));
 }
