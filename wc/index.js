5dddaae5250cdd2de3e5f403c0a8bcfe38194749
Prateek Mathur <prateekmathur1991@gmail.com>
2026-05-30 18:59:02 +0530

    Support the version where no flag is passed, defaulting to -c, -l, -w mode

--- wc/index.js
+++ wc/index.js
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
 
 const fs = require('fs');
-const readline = require('readline'); 
+const readline = require('readline');
 
-async function getFileStats(somePath) {
+async function getFileSize(somePath) {
   let stats = await fs.promises.stat(somePath);
   return stats.size;
 }
@@ -12,13 +12,13 @@ async function getFileLineCount(somePath) {
   return new Promise((resolve, reject) => {
     let lineCount = 0;
     fs.createReadStream(somePath)
-    .on('data', (buffer) => {
-      for (let i = 0; i < buffer.length; i++) {
+      .on('data', (buffer) => {
+        for (let i = 0; i < buffer.length; i++) {
           if (buffer[i] === 10) lineCount++; // 10 is '\n'
-      }
-    })
-    .on('end', () => resolve(lineCount))
-    .on('error', () => reject("Buffer reading failed"));
+        }
+      })
+      .on('end', () => resolve(lineCount))
+      .on('error', () => reject("Buffer reading failed"));
   });
 }
 
@@ -45,44 +45,55 @@ async function getCharacterCount(somePath) {
   return new Promise((resolve, reject) => {
     let totalChars = 0;
     fs.createReadStream(somePath)
-    .on('data', (chunk) => {
-      totalChars += [...chunk].length;
-    })
-    .on('end', () => resolve(totalChars))
-    .on('error', () => reject("Buffer reading failed"));
+      .on('data', (chunk) => {
+        totalChars += [...chunk].length;
+      })
+      .on('end', () => resolve(totalChars))
+      .on('error', () => reject("Buffer reading failed"));
   });
 }
 
 const args = process.argv.slice(2);
-if (args.length != 2) {
-  console.error('Usage: ccwc <flag> <fileName>');
+
+if (args.length == 0) {
+  console.error("Usage: ccwc <flag> <filename>");
   process.exit(1);
 }
 
-let flag = args[0];
-let filePath = args[1];
+if (args.length == 2) {
+  let flag = args[0];
+  let filePath = args[1];
 
-if (flag === '-c') {
-  (async () => {
-    const result = await getFileStats(filePath);
-    console.log(result + ' ' + filePath);
-  })();
-} else if (flag === '-l') {
-  (async () => {
-    let lineCount = await getFileLineCount(filePath);
-    console.log(lineCount + ' ' + filePath);
-  })();
-} else if (flag === '-w') {
-  (async () => {
-    let wordCount = await getWordCount(filePath);
-    console.log(wordCount + ' ' + filePath);
-  })();
-} else if (flag === '-m') {
-  (async () => {
-    let characterCount = await getCharacterCount(filePath);
-    console.log(characterCount + ' ' + filePath);
-  })();
+  if (flag === '-c') {
+    (async () => {
+      let fileSize = await getFileSize(filePath);
+      console.log(fileSize + ' ' + filePath);
+    })();
+  } else if (flag === '-l') {
+    (async () => {
+      let lineCount = await getFileLineCount(filePath);
+      console.log(lineCount + ' ' + filePath);
+    })();
+  } else if (flag === '-w') {
+    (async () => {
+      let wordCount = await getWordCount(filePath);
+      console.log(wordCount + ' ' + filePath);
+    })();
+  } else if (flag === '-m') {
+    (async () => {
+      let characterCount = await getCharacterCount(filePath);
+      console.log(characterCount + ' ' + filePath);
+    })();
+  } else {
+    console.error('Invalid flag used');
+    process.exit(1);
+  }
 } else {
-  console.error('Invalid flag used');
-  process.exit(1);
+  let filePath = args[0];
+  (async () => {
+    let fileSize = await getFileSize(filePath);
+    let lineCount = await getFileLineCount(filePath);
+    let wordCount = await getWordCount(filePath);
+    console.log(fileSize + ' ' + lineCount + ' ' + wordCount + ' ' + filePath);
+  })();
 }
