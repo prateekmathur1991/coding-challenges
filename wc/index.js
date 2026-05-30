50042657bc5802fc79496eeb93c2e979eab9dccc
Prateek Mathur <prateekmathur1991@gmail.com>
2026-05-30 12:57:27 +0530

    Can support reading line count from the file

--- wc/index.js
+++ wc/index.js
@@ -1,15 +1,25 @@
 #!/usr/bin/env node
 
-let fs = require('fs').promises;
+const fs = require('fs');
+const readline = require('readline'); 
 
-const fileStats = async (somePath) => {
-  let stats = await fs.stat(somePath);
+async function getFileStats(somePath) {
+  let stats = await fs.promises.stat(somePath);
   return stats.size;
 }
 
-async function getFileStats(somePath) {
-  let stats = await fs.stat(somePath);
-  return stats.size;
+async function getFileLineCount(somePath) {
+  return new Promise((resolve, reject) => {
+    let lineCount = 0;
+    fs.createReadStream(somePath)
+    .on('data', (buffer) => {
+      for (let i = 0; i < buffer.length; i++) {
+          if (buffer[i] === 10) lineCount++; // 10 is '\n'
+      }
+    })
+    .on('end', () => resolve(lineCount))
+    .on('error', () => reject("Buffer reading failed"));
+  });
 }
 
 const args = process.argv.slice(2);
@@ -24,9 +34,15 @@ let filePath = args[1];
 if (flag === '-c') {
   (async () => {
     const result = await getFileStats(filePath);
-    console.log(result);
+    console.log(result + ' ' + filePath);
+  })();
+
+} else if (flag === '-l') {
+  (async () => {
+    let lineCount = await getFileLineCount(filePath);
+    console.log(lineCount + ' ' + filePath);
   })();
 } else {
-  console.error('Invalid flag. Use -c for character count.');
+  console.error('Invalid flag used');
   process.exit(1);
 }
